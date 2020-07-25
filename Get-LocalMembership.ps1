40d4bc13cd7d2338e1ebd2fd0a0bb4457df6d094
Daniel Netzel <58021564+D-Netz@users.noreply.github.com>
2020-07-25 15:23:59 -0400

    Create Get-LocalMembership.ps1

---
+++ Get-LocalMembership.ps1
@@ -0,0 +1,37 @@
+<#
+checks local group membership for users that are enabled in the local machine
+I will be updating to:
+- make it into a function 
+- allow pipeline input
+- make it easier to work with pipline objects overall
+#>
+
+$localusr = get-localuser -name * | 
+Where-Object {$_.Enabled -eq $true -and $_.ObjectClass -match "([u,U]ser)"}
+
+$groups = @("Administrators", "Users")
+
+try {
+    ForEach ($group in $groups) {   
+        $params = @{
+            Group = $group ;
+            Member = $localusr.Name ;
+            ErrorAction = "Stop"
+        }
+        $properties = @{
+            Property = @{Name='GroupName'; Expression={$group}},
+                       "Name",
+                       "ObjectClass",
+                       #"PrincipalSource",
+                       @{Name='InGroup?'; Expression={$true}}
+        }
+        Get-LocalGroupMember @params | Select-Object @properties  
+    }
+}
+catch [Microsoft.PowerShell.Commands.PrincipalNotFoundException]{
+    "-" * 50
+    Write-Output "`nPrinicpal $localusr was not found in the $group group"
+}
+catch {
+    Write-Output $PSItem.Exception.Message -ForegroundColor Red
+}
