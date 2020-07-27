f75922d762257b137bd225d9f7c26392d029b4c2
Daniel Netzel <58021564+D-Netz@users.noreply.github.com>
2020-07-27 16:33:09 -0400

    Update Get-LocalMembership.ps1

--- Get-LocalMembership.ps1
+++ Get-LocalMembership.ps1
@@ -6,35 +6,42 @@ I will be updating to:
 - make it easier to work with pipline objects overall
 #>
 
-$localusr = get-localuser -name * | 
-Where-Object {$_.Enabled -eq $true -and $_.ObjectClass -match "([u,U]ser)"}
-
-$groups = @("Administrators", "Users")
-
 try {
-    ForEach ($group in $groups) {   
+    $MachineName = $ENV:COMPUTERNAME
+    $NameQuery = "SELECT * FROM Win32_UserAccount WHERE localaccount = true AND disabled = false"
+    $GroupQuery = "SELECT * from Win32_GroupUser"
+
+    $UserInfo = Get-CimInstance -ComputerName $MachineName -Query $NameQuery
+
+    $UserList = $UserInfo.name
+    
+    foreach ($user in $UserList) {
         $params = @{
-            Group = $group ;
-            Member = $localusr.Name ;
+            ComputerName = $MachineName;
+            Query = $GroupQuery;
             ErrorAction = "Stop"
         }
+
+        $Membership = Get-CimInstance @params `
+        | Where-Object {$_.PartComponent.Name -eq $user}
+        
+        $NameCheck = $Membership.PartComponent.name -eq $user
+        $GroupList = $Membership.GroupComponent.Name
         $properties = @{
-            Property = @{Name='GroupName'; Expression={$group}},
-                       "Name",
-                       "ObjectClass",
-                       #"PrincipalSource",
-                       @{Name='InGroup?'; Expression={$true}}
+            Property = @{Label='Domain';Expression={$Membership.PartComponent.Domain}}, `
+                    @{Label='Name';Expression={$user}}, `
+                    @{Label='GroupName';Expression={$_}}, `
+                    @{Label="InGroup?";Expression={$NameCheck}}
         }
-        Get-LocalGroupMember @params | Select-Object @properties  
+        
+        $GroupList | Select-Object @properties   
     }
 }
-catch [Microsoft.PowerShell.Commands.PrincipalNotFoundException]{
-    "-" * 50
-    Write-Output "`nPrinicpal $localusr was not found in the $group group"
-}
 catch {
-    Write-output $PSItem.Exception.Message
+    Write-Host "An Error Occured:" -ForegroundColor Red
+    Write-host $_.ScriptStackTrace "----->" $_.CategoryInfo.TargetName $_.CategoryInfo.Category -Foregroundcolor Blue
+    Write-Host $_.Exception.Message -ForegroundColor Red
 }
 finally {
-    $Error.Clear()
+    $error.Clear()
 }
