7112065762ceb7808bb1063e00e75ee08aa880de
Daniel Netzel <58021564+D-Netz@users.noreply.github.com>
2024-09-03 22:40:07 -0400

    Update Test-DCConnection.ps1
    edited comments

--- Windows/Test-DCConnection.ps1
+++ Windows/Test-DCConnection.ps1
@@ -2,7 +2,7 @@
 
 #Checks if DC01 can be resolved from DC02
  Resolve-DnsName `
- -Name DC01.netzel.lan `
+ -Name DC01.contoso.com `
  -Server DC01 `
  -Type A
  
@@ -10,11 +10,11 @@
  # 445 (SMB over IP) &  389 (LDAP). 
  # Used to access group policy details by joined PC's.
 Test-NetConnection `
--ComputerName DC01.netzel.lan `
+-ComputerName DC01.contoso.com `
  -Port 445
 
 Test-NetConnection `
--ComputerName DC01.netzel.lan `
+-ComputerName DC01.contoso.com`
 -Port 389
 
 #ping and nslookup can also be used.
